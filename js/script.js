c810f57c0590f93eeddd70632271966a07857e1a
Caleb Hsu <chialo.hsu@colorado.edu>
2016-04-28 00:42:04 -0600

    Added transitions for hover

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
-    $("#splash").click(function(){
-      $("#splash").fadeOut(2000, function() {
-        $("main").show(); 
-      });
+  $("#splash").click(function(){
+    $("#splash").fadeOut(2000, function() {
+      $("main").show(); 
     });
+  });
 
   /* Dropdown Toggles */
   $("#first").click(function(){
@@ -15,4 +15,11 @@ $(document).ready(function(){
   $("#third").click(function(){
       $(".stanza3").slideToggle("slow");
   });
+
+  $(".box").hover(function(){
+    $(this).css({"background-color": "rgba(255, 255, 255, 0.7)"});
+    }, function(){
+      $(this).css({"background-color": "rgba(255, 255, 255, 0.5)", "color": "black"});
+    });
 });
+
